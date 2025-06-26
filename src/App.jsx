f38e9689cd2b5dda734f723198254c90b9647941
mhamzamajeed <hamzamajeed190@gmail.com>
2025-06-26 17:03:06 +0500

    Hamza

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Quiz from './Components/Quiz/Quiz';
+import './index.css'
+
+  const App = () => {
+  return (
+  <>
+    <Quiz/>
+  </>
+  )
+}
+export default App;
